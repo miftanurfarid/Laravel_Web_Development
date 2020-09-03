be3f99a6da85ab1769b2ef18ae6c80157e6fa328
miftanurfarid <miftanurfarid@gmail.com>
2020-09-03 19:36:56 +0800

    materi konstruktor

---
+++ week2/day4/materi/sekolah_koding/robot.php
@@ -0,0 +1,41 @@
+<?php
+
+class robot{
+    
+  // property
+  public $suara;
+  public $berat;
+  
+  // konstruktor
+  public function __construct($suaranya, $beratnya) {
+    $this -> suara = $suaranya;
+    $this -> berat = $beratnya;
+  }
+
+  // metode set && metode get
+  // metode set : mengoper parameter dan merubah propertinya
+  public function set_suara($suaranya) {
+    $this -> suara = $suaranya;
+  }
+  
+  public function get_suara() {
+    return $this -> suara;
+  }
+
+  public function set_berat($beratnya) {
+    $this -> berat = $beratnya;
+  }
+
+  public function get_berat() {
+    return $this -> berat;
+  }
+}
+
+// inheritance atau pewarisan
+class robot_hewan extends robot {
+
+  public function get_kekuatan() {
+    echo "saya hewan dahsyaat!<br>";
+  }
+}
+?>
