28921f6609a6be6f1eb76fd907081af9e9d7480a
miftanurfarid <miftanurfarid@gmail.com>
2020-09-04 22:42:34 +0800

    tambah robot2

--- week2/day4/materi/sekolah_koding/6_overriding_method.php
+++ week2/day4/materi/sekolah_koding/6_overriding_method.php
@@ -1,11 +1,16 @@
+<!-- inheritance atau pewarisan -->
 <?php
 
   require_once 'robot.php';
   require_once 'hewan.php';
 
   $robot1 = new robot('anjaaay!!', '80 kg!!');
-  
   echo 'bunyi robotku ... ' . $robot1 -> get_suara() . ' dan beratnya adalah ' . $robot1 -> get_berat();
+  
+  echo "<br>";
 
+  $robot2 = new robot_hewan('ngiing~~', '15 kg');
+  echo $robot2 -> get_suara();
+  echo $robot2 -> get_kekuatan();
   
 ?>
