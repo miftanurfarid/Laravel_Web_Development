31dea9d5a486d23af885463aadc31c7214d59ace
miftanurfarid <miftanurfarid@gmail.com>
2020-09-04 22:42:23 +0800

    tambah require_once

--- week2/day4/materi/sekolah_koding/5_inheritance.php
+++ week2/day4/materi/sekolah_koding/5_inheritance.php
@@ -2,6 +2,7 @@
 <?php
 
   require_once 'robot.php';
+  require_once 'hewan.php';
 
   $robot1 = new robot('anjaaay!!', '80 kg!!');
   echo 'bunyi robotku ... ' . $robot1 -> get_suara() . ' dan beratnya adalah ' . $robot1 -> get_berat();
